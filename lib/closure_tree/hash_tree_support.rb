02689da3f186b7412adf648d298afc0614123631
Tom Smyth <tom@sassafras.coop>
2016-08-04 14:36:18 -0400

    Temporary fix of hash_tree bug

--- lib/closure_tree/hash_tree_support.rb
+++ lib/closure_tree/hash_tree_support.rb
@@ -1,24 +1,33 @@
 module ClosureTree
   module HashTreeSupport
-    def default_tree_scope(scope, limit_depth = nil)
+    def default_tree_scope(limit_depth = nil)
         # Deepest generation, within limit, for each descendant
         # NOTE: Postgres requires HAVING clauses to always contains aggregate functions (!!)
         having_clause = limit_depth ? "HAVING MAX(generations) <= #{limit_depth - 1}" : ''
-        generation_depth = <<-SQL.strip_heredoc
-          INNER JOIN (
-            SELECT descendant_id, MAX(generations) as depth
-            FROM #{quoted_hierarchy_table_name}
-            GROUP BY descendant_id
-            #{having_clause}
-          ) AS generation_depth
-            ON #{quoted_table_name}.#{model_class.primary_key} = generation_depth.descendant_id
-        SQL
-        scope_with_order(scope.joins(generation_depth), 'generation_depth.depth')
+        add_depth_order(model_class, having_clause)
       end
 
     def hash_tree(tree_scope, limit_depth = nil)
-      limited_scope = limit_depth ? tree_scope.where("#{quoted_hierarchy_table_name}.generations <= #{limit_depth - 1}") : tree_scope
+      limited_scope = if tree_scope
+        tree_scope = tree_scope.where("#{quoted_hierarchy_table_name}.generations <= #{limit_depth - 1}") if limit_depth
+        add_depth_order(tree_scope)
+      else
+        default_tree_scope(limit_depth)
+      end
       build_hash_tree(limited_scope)
+    end
+
+    def add_depth_order(scope, having_clause = '')
+      generation_depth = <<-SQL.strip_heredoc
+        INNER JOIN (
+          SELECT descendant_id, MAX(generations) as depth
+          FROM #{quoted_hierarchy_table_name}
+          GROUP BY descendant_id
+          #{having_clause}
+        ) AS generation_depth
+          ON #{quoted_table_name}.#{model_class.primary_key} = generation_depth.descendant_id
+      SQL
+      scope_with_order(scope.joins(generation_depth), 'generation_depth.depth')
     end
 
     # Builds nested hash structure using the scope returned from the passed in scope
